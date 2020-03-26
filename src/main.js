9fce1dce76a17473e257d395b34be0493f2321c7
TheQuail13 <dougthierjung@gmail.com>
2020-03-26 07:59:02 -0700

    Step 1: Librarification

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import VueCardCarousel from './components/VueCardCarousel.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+export default VueCardCarousel;
