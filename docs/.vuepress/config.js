f44e5bb14dc18faf911b801f4ff96d7be6f55917
TheQuail13 <dougthierjung@gmail.com>
2020-03-31 16:02:26 -0700

    added super basic vuepress doc configs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'VueCardCarousel',
+    themeConfig: {
+    nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' }
+    ],
+    repo: 'TheQuail13/vue-card-carousel',
+    lastUpdated: 'Last Updated'
+    }
+}
